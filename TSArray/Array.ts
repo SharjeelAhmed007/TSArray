7abe9ba880cbd28701c278a7fdc025384b1ffea1
SharjeelAhmed007 <java1234>
2015-08-24 14:04:15 -0700

    Array using TS

---
+++ TSArray/Array.ts
@@ -0,0 +1,8 @@
+/*Created by:Sharjeel Ahmed */
+/*var array:any []=["sharjeel",2,false];
+document.writeln(array[0]);
+*/
+var student={id:1 ,name:"sharjeel",class:"BSSE" };
+document.getElementById("stuid").innerHTML="student id :"+student.id;
+document.getElementById("stuName").innerHTML="student Name :"+student.name;
+document.getElementById("StuClass").innerHTML="student Class :"+student.class;
